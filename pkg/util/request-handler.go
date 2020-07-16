801efa82fee28e9ca2cd58180eacb793ce2f5add
xiang <dryadf66449@gmail.com>
2020-07-16 11:42:42 +0800

    更新 requester 方法，新增加入 header

--- pkg/util/request-handler.go
+++ pkg/util/request-handler.go
@@ -50,6 +50,11 @@ type RequesterInterface interface {
 	PUT(uri string, param interface{}) (*e.Response, error)
 	PATCH(uri string, param interface{}) (*e.Response, error)
 	DELETE(uri string, param interface{}) (*e.Response, error)
+	GetWithHeader(uri string, header map[string]string) (*e.Response, error)
+	PostWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error)
+	PutWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error)
+	PatchWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error)
+	DeleteWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error)
 }
 
 type Requester struct {
@@ -84,7 +89,6 @@ func (r *Requester) GET(uri string) (*e.Response, error) {
 	}
 	return r.ReadJSONResponse(resp)
 }
-
 func (r *Requester) POST(uri string, param interface{}) (*e.Response, error) {
 	// 轉換參數
 	jsonByte, _ := json.Marshal(param)
@@ -141,6 +145,89 @@ func (r *Requester) DELETE(uri string, param interface{}) (*e.Response, error) {
 	req, err := http.NewRequest(http.MethodDelete, uri, bytes.NewReader(jsonByte))
 	if err != nil {
 		return nil, err
+	}
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return r.ReadJSONResponse(resp)
+}
+
+func (r *Requester) GetWithHeader(uri string, header map[string]string) (*e.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range header {
+		req.Header.Set(key, value)
+	}
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return r.ReadJSONResponse(resp)
+}
+func (r *Requester) PostWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error) {
+	// 轉換參數
+	jsonByte, _ := json.Marshal(param)
+	// 將 json data 放在 body 進行 request
+	req, err := http.NewRequest(http.MethodPost, uri, bytes.NewReader(jsonByte))
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range header {
+		req.Header.Set(key, value)
+	}
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return r.ReadJSONResponse(resp)
+}
+func (r *Requester) DeleteWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error) {
+	// 轉換參數
+	jsonByte, _ := json.Marshal(param)
+	// 將 json data 放在 body 進行 request
+	req, err := http.NewRequest(http.MethodDelete, uri, bytes.NewReader(jsonByte))
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range header {
+		req.Header.Set(key, value)
+	}
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return r.ReadJSONResponse(resp)
+}
+func (r *Requester) PutWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error) {
+	// 轉換參數
+	jsonByte, _ := json.Marshal(param)
+	// 將 json data 放在 body 進行 request
+	req, err := http.NewRequest(http.MethodPut, uri, bytes.NewReader(jsonByte))
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range header {
+		req.Header.Set(key, value)
+	}
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return r.ReadJSONResponse(resp)
+}
+func (r *Requester) PatchWithHeader(uri string, header map[string]string, param interface{}) (*e.Response, error) {
+	// 轉換參數
+	jsonByte, _ := json.Marshal(param)
+	// 將 json data 放在 body 進行 request
+	req, err := http.NewRequest(http.MethodPatch, uri, bytes.NewReader(jsonByte))
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range header {
+		req.Header.Set(key, value)
 	}
 	resp, err := r.client.Do(req)
 	if err != nil {
