e8be5196137419a1e19bb514eb08fabe17e6960a
xiang <dryadf66449@gmail.com>
2020-12-03 19:10:37 +0800

    加入分頁

--- v2/util/util/pagination.go
+++ v2/util/util/pagination.go
@@ -28,3 +28,32 @@ func Pagination(in *gorm.DB, data map[string]interface{}) (*gorm.DB, map[string]
 		return in.Limit(pageSize).Offset((page - 1) * pageSize), data
 	}
 }
+func PaginationScope(data map[string]interface{}) func(in *gorm.DB) *gorm.DB {
+	return func(in *gorm.DB) *gorm.DB {
+		if data == nil {
+			return in
+		} else if data[pagination.PAGE_SIZE] == nil && data[pagination.PAGE] == nil {
+			return in
+		} else {
+			var (
+				pageSize = 10
+				page     = 1
+			)
+
+			if in := data[pagination.PAGE_SIZE]; in != nil {
+				pageSize, _ = strconv.Atoi(in.(string))
+				delete(data, pagination.PAGE_SIZE)
+			}
+			if in := data[pagination.PAGE]; in != nil {
+				page, _ = strconv.Atoi(in.(string))
+				delete(data, pagination.PAGE)
+			}
+			return in.Limit(pageSize).Offset((page - 1) * pageSize)
+		}
+	}
+}
+func TotalSize(in *gorm.DB) int {
+	total := 0
+	in.Count(&total)
+	return total
+}
