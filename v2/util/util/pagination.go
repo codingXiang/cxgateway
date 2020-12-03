1ddf4ac73a70bb82532d08aaa83aae5e87a31bb7
xiang <dryadf66449@gmail.com>
2020-12-03 19:17:48 +0800

    優化分頁機制

--- v2/util/util/pagination.go
+++ v2/util/util/pagination.go
@@ -6,29 +6,29 @@ import (
 	"strconv"
 )
 
-func Pagination(in *gorm.DB, data map[string]interface{}) (*gorm.DB, map[string]interface{}) {
-	if data == nil {
-		return in, data
-	} else if data[pagination.PAGE_SIZE] == nil && data[pagination.PAGE] == nil {
-		return in, data
-	} else {
-		var (
-			pageSize = 10
-			page     = 1
-		)
-
-		if in := data[pagination.PAGE_SIZE]; in != nil {
-			pageSize, _ = strconv.Atoi(in.(string))
-			delete(data, pagination.PAGE_SIZE)
-		}
-		if in := data[pagination.PAGE]; in != nil {
-			page, _ = strconv.Atoi(in.(string))
-			delete(data, pagination.PAGE)
-		}
-		return in.Limit(pageSize).Offset((page - 1) * pageSize), data
-	}
-}
-func PaginationScope(data map[string]interface{}) func(in *gorm.DB) *gorm.DB {
+//func Pagination(in *gorm.DB, data map[string]interface{}) (*gorm.DB, map[string]interface{}) {
+//	if data == nil {
+//		return in, data
+//	} else if data[pagination.PAGE_SIZE] == nil && data[pagination.PAGE] == nil {
+//		return in, data
+//	} else {
+//		var (
+//			pageSize = 10
+//			page     = 1
+//		)
+//
+//		if in := data[pagination.PAGE_SIZE]; in != nil {
+//			pageSize, _ = strconv.Atoi(in.(string))
+//			delete(data, pagination.PAGE_SIZE)
+//		}
+//		if in := data[pagination.PAGE]; in != nil {
+//			page, _ = strconv.Atoi(in.(string))
+//			delete(data, pagination.PAGE)
+//		}
+//		return in.Limit(pageSize).Offset((page - 1) * pageSize), data
+//	}
+//}
+func Pagination(data map[string]interface{}) func(in *gorm.DB) *gorm.DB {
 	return func(in *gorm.DB) *gorm.DB {
 		if data == nil {
 			return in
